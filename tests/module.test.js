c30726cd741ddfe6ece0bf0ce1fc93cf83c95224
ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDFCpkaQgLdANfmSqQ65NlnJ1Q592NjLqG5iHhyENjG9HTPNUSdX8y+sjvKnb7h1B1P2s9jPnlVG3aXb22rK30MG0Y4dCLMwL20FMGc7+4jYH70S8OxetWUgxY7y9/5xdmAJ3AWvuuMNf9s0KMKIfYJl85V8kTAub6JsH/pSlCtT1U56Kt1ao0ipV/o6evBsDV/R1X9gfOspgx53DKuwl//goanRAtqXoEURFW/nt4L8DPfBodtMS+OBtIqR3SobFSd0bChP2PxUD5g08QfYyonX1MZHYWJqEq/T8N/K0N9nfuDjEBm62ELvrK4MWosJuwFLOx+UyzyjIRdsx6NSIf+KIOtWLtKx4ldUWzDhrJyN6m3jNT9JRArh1yO61nZfojLgZ6HKoUYSR85MeuZ4ms+sH7WZL7e7Ht5u/gQi5hBT0cvA4Opky8l4uo7tdi9RUMXZKYHUGIZRzUiQAFRjSMdJ8cUTZmhv6OtLDe/NKEa+m5rBrrdyrVJVPP/C8d/Tlk= wdbailey87@gmail.com <wdbailey87@gmail.com>
2021-09-23 17:48:24 -0700

    testing is fun

---
+++ tests/module.test.js
@@ -0,0 +1,16 @@
+const chai = require('chai')
+const assert = chai.assert
+const expect = chai.expect
+const { hello } = require('../src/scripts/module')
+
+describe('hello world module', () => {
+  const helloWorld = hello()
+
+  it('should return a string', () => {  
+    assert.typeOf(helloWorld, 'string')
+  })
+
+  it('should equal "hello from a module"', () => {
+    assert.equal(helloWorld, 'hello from a module')
+  })
+})
