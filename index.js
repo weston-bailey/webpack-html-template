b6432d0c176cf755a746d92d7724b86abac4572f
ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDFCpkaQgLdANfmSqQ65NlnJ1Q592NjLqG5iHhyENjG9HTPNUSdX8y+sjvKnb7h1B1P2s9jPnlVG3aXb22rK30MG0Y4dCLMwL20FMGc7+4jYH70S8OxetWUgxY7y9/5xdmAJ3AWvuuMNf9s0KMKIfYJl85V8kTAub6JsH/pSlCtT1U56Kt1ao0ipV/o6evBsDV/R1X9gfOspgx53DKuwl//goanRAtqXoEURFW/nt4L8DPfBodtMS+OBtIqR3SobFSd0bChP2PxUD5g08QfYyonX1MZHYWJqEq/T8N/K0N9nfuDjEBm62ELvrK4MWosJuwFLOx+UyzyjIRdsx6NSIf+KIOtWLtKx4ldUWzDhrJyN6m3jNT9JRArh1yO61nZfojLgZ6HKoUYSR85MeuZ4ms+sH7WZL7e7Ht5u/gQi5hBT0cvA4Opky8l4uo7tdi9RUMXZKYHUGIZRzUiQAFRjSMdJ8cUTZmhv6OtLDe/NKEa+m5rBrrdyrVJVPP/C8d/Tlk= wdbailey87@gmail.com <wdbailey87@gmail.com>
2021-09-24 13:47:31 -0700

    add error handling to express app

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const path = require('path')
-const express = require('express')
+const express = require('express');
 const app = express()
 
 app.use(express.static(path.join(__dirname, 'dist')));
 app.set('port', process.env.PORT || 3000);
+
+app.get('/error-test', () => {
+  throw new Error('test error')
+})
+
+app.use((req, res) => {
+  res.status(404).send(`cannot ${req.method} ${req.url}`)
+})
+
+app.use((err, req, res, next) => {
+  console.log(err)
+  res.status(500).send('internal server error')
+})
 
 const server = app.listen(app.get('port'), err => {
   if (err) return console.log(err)
