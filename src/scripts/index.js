d1a426d4dea417d58c253715062e7c7da4c87779
weston-bailey <wdbailey87@gmail.com>
2022-09-10 20:01:14 -0700

    fix json import

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,6 +2,9 @@
 const { hello } = require('./module')
 // css file just needs to be required in the main js
 require('../styles/index.css')
+const json = require('./myJson.json')
+
+console.log(json)
 
 console.log('hello from the index')
 console.log(hello())
@@ -9,3 +12,5 @@ console.log(hello())
 // regular DOM still works!
 const myBody = document.querySelector('body')
 console.log(myBody)
+
+
--- webpack.config.js
+++ webpack.config.js
@@ -77,11 +77,6 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
-      },
-      // json loader TODO: fix
-      {
-        test: /\.json$/,
-        loader: 'json-loader'
       }
     ]
   },
